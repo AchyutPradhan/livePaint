e5a0945f0f09fe66d8d7b45c8e60e24c29b8ce9b
Vikram-Pavurayala <127219131+Vikram-Pavurayala@users.noreply.github.com>
2024-04-29 22:38:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-
+let userCount=0;
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
   console.log('A user connected');
-
+  userCount++;
+  io.emit('userCount', userCount);
+  console.log(userCount);
   // Broadcast drawing data to all other clients
   socket.on('draw', (data) => {
     socket.broadcast.emit('draw', data);
@@ -16,7 +18,12 @@ io.on('connection', (socket) => {
   // Handle disconnection
   socket.on('disconnect', () => {
     console.log('A user disconnected');
+    userCount--;
+    io.emit('userCount', userCount);
+    console.log(userCount);
   });
+
+  
 });
 
 const PORT = process.env.PORT || 3000;
